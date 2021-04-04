1d0592ef36c71b7d97ec22dbb988da2483df591d
UDQD <70804333+UDQD@users.noreply.github.com>
2021-04-04 17:16:00 +0300

    04.04.2021
    + префикс
    + периодическое обновление базы

--- main.py
+++ main.py
@@ -1,42 +1,33 @@
 import nest_asyncio
 import discord
-
+from currency import get_cur
+from table import update
 # from bs4 import BeautifulSoup
 
+TOKEN = "ODEwMDg4ODQ3MDk3NDYyNzk2.YCekBw.7MADVjHD6NQdJiuc8-ZYQz6KS9o"
 
-from bot_token import TOKEN
-# from parse import get_voltage
-from currency import get_cur
+
 
 nest_asyncio.apply()
 intents = discord.Intents.default()
 intents.members = True  # Subscribe to the privileged members intent.
 client = discord.Client(intents=intents)
 
-
-# def get_dollar():
-#     html = requests.get('https://www.cbr.ru/').text
-#     soup = BeautifulSoup(html)
-#     mydivs = soup.findAll("div", {"class": "col-md-2 col-xs-9 _right mono-num"})
-#     return (mydivs[0].text)
-
-
 @client.event
 async def on_ready():
     print('We have logged in as {0.user}'.format(client))
+    update()
 
 
 @client.event
 async def on_message(message):
+
     if message.author == client.user:
         return
-    if message.content.startswith('курс'):
-        print(message.content.split(' ')[1])
-        d = len(message.content.split(' '))
-        if d == 2:
-            await message.channel.send(get_cur(message.content.split(' ')[1]))
-        else:
-            await message.channel.send('неверный запрос')
+
+    if message.content.startswith('!'):
+        await message.channel.send(get_cur(message.content[1:]))
+        print(message.content[1:])
 
 
 client.run(TOKEN)
