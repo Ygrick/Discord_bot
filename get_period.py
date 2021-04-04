e4353b5d0eb7fecde3c24d2f43efffaea708d90c
UDQD <70804333+UDQD@users.noreply.github.com>
2021-04-04 22:51:50 +0300

    + графики

--- get_period.py
+++ get_period.py
@@ -1,5 +1,7 @@
 import requests
 import pandas as pd
+from getRcode import getRcode
+from matplotlib import pyplot as plt
 
 
 # на вход с какого по какое число и Rcode валюты
@@ -11,8 +13,34 @@ def get_period(fromDate, toDate, Rcode):
     table = ((pd.concat(pd.read_html(html), ignore_index = True)).drop(index=[0,1])).reset_index(drop=True)
     table.columns = ['date', 'multiplier', 'rate']
     table['rate'], table['multiplier'] = pd.to_numeric(table['rate'],downcast = "float"), pd.to_numeric(table['multiplier'])
-    table['date'] = pd.to_datetime(table['date'])
+    table['date'] = pd.to_datetime(table['date'],format="%d.%m.%Y")
     for n in range(table.shape[0]):
         table['rate'][n] /= table['multiplier'][n]*10000
     table = table.drop('multiplier', axis=1)
+    table = table.iloc[::-1].reset_index(drop=True)
     return table
+
+def get_image(curr, fromDate, toDate):
+    # curr = curr[0].upper() + curr[1:].lower()
+    Rcode = getRcode(curr)
+    # return Rcode
+    print(fromDate, toDate, Rcode)
+    df = get_period(fromDate, toDate, Rcode)
+    # import numpy as np
+
+
+    x = df.date
+    y = df.rate
+
+    fig, ax = plt.subplots()
+
+    ax.plot(x, y)
+
+    ax.xaxis_date()  # interpret the x-axis values as dates
+    fig.autofmt_xdate()  # make space for and rotate the x-axis tick labels
+    plt.fill(x, y, '#00a1e4', alpha=0.3)
+    # plt.figure(figsize=(20, 20))
+    # plt.show()
+
+    fig.savefig('graf.jpg')
+    return 'graf.jpg'
--- main.py
+++ main.py
@@ -1,39 +1,50 @@
 import nest_asyncio
 import discord
 from currency import get_cur
-
 from table import update
-
 from table import get_voltage
+from get_period import get_image
 
 # from bs4 import BeautifulSoup
 
 TOKEN = "ODEwMDg4ODQ3MDk3NDYyNzk2.YCekBw.7MADVjHD6NQdJiuc8-ZYQz6KS9o"
 
-
+prefix = '!'
 
 nest_asyncio.apply()
 intents = discord.Intents.default()
 intents.members = True  # Subscribe to the privileged members intent.
 client = discord.Client(intents=intents)
 
+
 @client.event
 async def on_ready():
     print('We have logged in as {0.user}'.format(client))
-
     update()
 
 
 @client.event
 async def on_message(message):
-
     if message.author == client.user:
         return
 
+    if message.content.startswith(prefix + 'cur'):
+        await message.channel.send(get_cur(message.content.split()[1]))
+        print(message.content.split()[1])
 
-    if message.content.startswith('!'):
-        await message.channel.send(get_cur(message.content[1:]))
-        print(message.content[1:])
+    if message.content.startswith(prefix + 'range'):
+        content = message.content.split()
+        ln = len(content)
+        curr = content[1:ln-2]
+        curr = ' '.join(curr)
+        start_date = content[-2]
+        end_date = content[-1]
+        img = get_image(curr, start_date, end_date)
+        await message.channel.send(file=discord.File(img))
+        # print(content)
+        print(curr)
+        # await message.channel.send(img)
 
 
-client.run(TOKEN)
+if __name__ == "__main__":
+    client.run(TOKEN)
