9e85df02cc81e05bf53cfc59ecef12a68d958762
UDQD <70804333+UDQD@users.noreply.github.com>
2021-04-04 17:53:50 +0300

    + префикс + периодическое обновление базы

--- currency.py
+++ currency.py
@@ -1,12 +1,12 @@
-
 from table import get_voltage
+from table import TABLE
 
 
 def get_cur(message):
-    # фывфвы
-    df_modified = get_voltage().set_index('name_key')
-    df_modified_number_key = get_voltage().set_index('number_key')
-    df_modified_name_currency = get_voltage().set_index('name_currency')
+
+    df_modified = TABLE.set_index('name_key')
+    df_modified_number_key = TABLE.set_index('number_key')
+    df_modified_name_currency = TABLE.set_index('name_currency')
 
     try:
         return str(df_modified.loc[message, 'rate']) + ' рублей'
